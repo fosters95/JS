2f4fdf950b7edd5ae786a9440557b2fbc827be43
fosters95 <fosters95@mail.ru>
2022-09-03 17:49:06 +0300

    for

--- Js/alert.js
+++ Js/alert.js
@@ -1,19 +1,8 @@
 "use strict";
-let login = prompt("Кто там?");
-if (login === "admin") {
-	let password = prompt("Пароль?");
-	if (password === "Я Главный") {
-		alert("Здравствуйте!");
-	} else if (password === "" || password === null) {
-		alert("Отменено");
-	} else {
-		alert("Неверный пароль");
-	}
-} else if (login === "" || login === null) {
-		alert("Отменено");
-	} else {
-		alert("Я вас не знаю");
-	}
+for (let i = 2; i <= 10; i++) {
+	if (i % 2 !== 0) continue;
+	alert(i);
+}
 
 
 

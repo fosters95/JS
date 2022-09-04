5b7b370a0bf49b52d34e93e34f72a26887750e31
fosters95 <fosters95@mail.ru>
2022-09-04 11:57:33 +0300

    if to switch

--- Js/alert.js
+++ Js/alert.js
@@ -1,10 +1,22 @@
 "use strict";
-let browser;
-if (browser == 'edge') {
-	alert ("You've got the Edge!");
-} else if (browser == 'Chrome' || 'Firefox' || 'Safari' || 'Opera') {
-	alert('Okay we support these browsers too');
+const number = +prompt('Введите число между 0 и 3', '');
+switch (number) {
+	case 0:
+		alert('Вы ввели число 0');
+		break;
+	case 1:
+		alert('Вы ввели число 1');
+		break
+	case 2:
+	case 3:
+		alert('Вы ввели число 2, а может и 3');
 }
-else {
-	alert('We hope that this page looks ok!');
-}
+
+
+
+
+
+
+
+
+

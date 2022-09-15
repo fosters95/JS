5da56ebae4cc9ac032ac75c4a91b91c8a5454d16
fosters95 <fosters95@mail.ru>
2022-09-15 21:12:12 +0300

    first lesson

--- Js/alert.js
+++ Js/alert.js
@@ -1,22 +1,23 @@
 "use strict";
-const number = +prompt('Введите число между 0 и 3', '0123');
-switch (number) {
-	case 0:
-		alert('Вы ввели число 0');
-		break;
-	case 1:
-		alert('Вы ввели число 1');
-		break
-	case 2:
-	case 3:
-		alert('Вы ввели число 2, а может и 3');
-}
+console.log('Hello!'); //строки берем в ковычки
+console.log('World');
+console.log('3431');
+console.log(3443);
+console.log('Hello '+'world'); //конкатенация
+console.info('Hello');
+// alert('Hello');
+// alert('Hello');
 
+document.getElementById("out").innerHTML = 'Hello';
+document.getElementById("out").innerHTML = 2022;
+document.getElementById("out").innerHTML = '<b>2020</b>';
+document.querySelector('.header').innerHTML = 5;
+document.querySelector('#one').innerHTML = 777 // есть решетка
+document.getElementById('one').innerHTML = 888 //нет решетки
 
-
-
-
-
-
-
-
+let b; // var - устарело
+let a = document.querySelector('#one'); // внутрь а получил параграф
+let c;
+c = document.querySelector('.header');
+a.innerHTML = 9999;
+c.innerHTML = 56565;

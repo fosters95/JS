738d2102f0b461f38c917d09b90dcfd41d2a9db8
fosters95 <fosters95@mail.ru>
2022-09-17 15:59:33 +0300

    fourth lesson

--- Js/alert.js
+++ Js/alert.js
@@ -1,40 +1,35 @@
 "use strict";
-let a = 9;
-
-// >= <= == (проверка равенства) != (не равно)
-// if (a != 9) {
-//     // выполнится если выражение в скобках true
-//     console.log('Yes');
-// } else {
-//     console.log('else');
+// document.querySelector('button').onclick = function (){
+//     console.log(document.querySelector('#one').value);
+//     // style
+//     document.querySelector('button').style.backgroundColor =
+//     document.querySelector('#one').value
+// }
+//
+// document.querySelector('#one').oninput = () => {
+//     console.log(document.querySelector('#one').value);
+//     document.querySelector('span').innerHTML = document.querySelector('#one').value
+//
 // }
 
-const button = document.querySelector('button');
-const input = document.querySelector('.age');
-
-button.onclick = () => { // стрелочная ф-ция (аналогично function ())
-    let num = +input.value;
-    if (num >= 16 && num < 60) {
-        console.log('Welcome!');
-    } else if (num > 60) {
-        console.log('Ты точно сюда??');
+document.querySelector('#btn-1').onclick =  () => {
+    console.log(document.querySelector('#i2').value);
+    let myCheckBox = document.querySelector('#i2');
+    console.log(myCheckBox.checked);
+    if (myCheckBox.checked) {
+        console.log('Нажат');
     } else {
-        console.log('Вход воспрещен!');
-    }
-
-
-    switch (num) {
-        case 15:
-            console.log('Потерпи еще год');
-            break;
-        case 16:
-            console.log('Yeahh!!');
-            break;
-        default:
-            console.log('gogoggo');
+        console.log('Не нажат');
     }
 }
 
-let b = 4;
-
-console.log (b == 3 || b == 7); //&& - и  || - или
+document.querySelector('#btn-2').onclick =  (event) => {
+    event.preventDefault(); //позволят остановить перезагрузку страницы при нажатии click
+    // let text = document.querySelector('#two');
+    // console.log(text.value);
+    // text.value = "one";
+    let form = document.querySelector('form');
+    console.log(form);
+    console.log(form.elements.n1.value);
+    console.log(form.elements.three.value);
+}

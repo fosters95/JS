9b514cb3b46d6ea14469ed8cac81f9a9d144fad2
fosters95 <fosters95@mail.ru>
2022-09-17 11:40:45 +0300

    second lesson

--- Js/alert.js
+++ Js/alert.js
@@ -1,23 +1,18 @@
 "use strict";
-console.log('Hello!'); //строки берем в ковычки
-console.log('World');
-console.log('3431');
-console.log(3443);
-console.log('Hello '+'world'); //конкатенация
-console.info('Hello');
-// alert('Hello');
-// alert('Hello');
+let a = 6;
+let b = 'Hello';
+let div = document.querySelector('.out')
 
-document.getElementById("out").innerHTML = 'Hello';
-document.getElementById("out").innerHTML = 2022;
-document.getElementById("out").innerHTML = '<b>2020</b>';
-document.querySelector('.header').innerHTML = 5;
-document.querySelector('#one').innerHTML = 777 // есть решетка
-document.getElementById('one').innerHTML = 888 //нет решетки
+console.log(a);
+let inputIn = document.querySelector('.input-in');
+let button = document.querySelector('button');
 
-let b; // var - устарело
-let a = document.querySelector('#one'); // внутрь а получил параграф
-let c;
-c = document.querySelector('.header');
-a.innerHTML = 9999;
-c.innerHTML = 56565;
+button.onclick = function (){
+    //кнопка будет нажата
+    console.log('Работает!');
+    // console.log(inputIn.value); // value - то что введено в инпут
+    let b = +inputIn.value; // + переводит из строки в число
+    console.log(b + 10);
+    div.innerHTML = b;
+    inputIn.value = '';
+}

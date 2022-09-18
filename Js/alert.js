0529f0be1876bfe05c1fed311b172bd058b3dbe1
fosters95 <fosters95@mail.ru>
2022-09-18 15:29:02 +0300

    first project

--- Js/alert.js
+++ Js/alert.js
@@ -1,35 +1,22 @@
 "use strict";
-// document.querySelector('button').onclick = function (){
-//     console.log(document.querySelector('#one').value);
-//     // style
-//     document.querySelector('button').style.backgroundColor =
-//     document.querySelector('#one').value
-// }
-//
-// document.querySelector('#one').oninput = () => {
-//     console.log(document.querySelector('#one').value);
-//     document.querySelector('span').innerHTML = document.querySelector('#one').value
-//
-// }
+let c = '(())';
+// цикл
+// c.length - длина строки
+console.log(c.length);
+let count = 0;
 
-document.querySelector('#btn-1').onclick =  () => {
-    console.log(document.querySelector('#i2').value);
-    let myCheckBox = document.querySelector('#i2');
-    console.log(myCheckBox.checked);
-    if (myCheckBox.checked) {
-        console.log('Нажат');
-    } else {
-        console.log('Не нажат');
+for (let i = 0; i < c.length; i++) {
+    // console.log(c[i]);
+    if (c[i] == '(') {
+        count++ // увеличиваем на 1
     }
+    if (c[i] == ')'){
+        count--;
+    }
+    if (count < 0) {
+        // console.log(false);
+        break;
+        }
 }
-
-document.querySelector('#btn-2').onclick =  (event) => {
-    event.preventDefault(); //позволят остановить перезагрузку страницы при нажатии click
-    // let text = document.querySelector('#two');
-    // console.log(text.value);
-    // text.value = "one";
-    let form = document.querySelector('form');
-    console.log(form);
-    console.log(form.elements.n1.value);
-    console.log(form.elements.three.value);
-}
+if (count != 0) console.log(false)
+else console.log(true);
